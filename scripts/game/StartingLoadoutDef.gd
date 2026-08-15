f5baea3e3305a04f92edfa6cd6b509ae94259cff
Cherry <949886@qq.com>
2026-08-15 11:54:51 +0800

    feat(game): add starting loadout resource

---
+++ scripts/game/StartingLoadoutDef.gd
@@ -0,0 +1,8 @@
+class_name StartingLoadoutDef
+extends Resource
+
+## Data-only definition of the equipment granted when a game starts.
+
+@export var starting_wands: Array[WandDef] = []
+@export var starting_spells: Array[SpellDef] = []
+@export var starting_gold: int = 0
